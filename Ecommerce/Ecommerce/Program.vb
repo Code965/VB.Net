b22a23ab9d0debc9539de3bfdf8f17787e6e6a69
Code965 <domenicoemanuele.giannone@gmail.com>
2024-07-21 09:22:04 +0200

    Messaggio di saluto

--- Ecommerce/Ecommerce/Program.vb
+++ Ecommerce/Ecommerce/Program.vb
@@ -2,6 +2,6 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("Ciao, siamo work in progress")
     End Sub
 End Module
